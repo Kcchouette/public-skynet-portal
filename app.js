018dc75bcf64721514922311e21e9dcf860ba619
Kcchouette <kcchouette@posteo.org>
2020-07-26 16:30:12 +0200

    Adding more debug log and mobile-improvement

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ function checkIsOnline(portal_url) {
     let startTime = (new Date()).getTime();
     requestPortalSkyHash.send();
     requestPortalSkyHash.onreadystatechange = function () {
+
+        console.debug(portal_url, this.readyState, requestPortalSkyHash.status, requestPortalSkyHash.getAllResponseHeaders(), requestPortalSkyHash.getResponseHeader("skynet-file-metadata"));
+
         if ((this.readyState === 2 || this.readyState === 4) && requestPortalSkyHash.status === 200) {   
             let endTime = (new Date()).getTime();
             document.getElementById(portal_url + "-duration").innerHTML = endTime - startTime;
-            console.debug(portal_url, requestPortalSkyHash.getAllResponseHeaders(), requestPortalSkyHash.getResponseHeader("skynet-file-metadata"));
             if(requestPortalSkyHash.getResponseHeader("skynet-file-metadata") === CONTENT_SKY_HASK) {
                 document.getElementById(portal_url + "-online").innerHTML = '&#9989;';
             }
@@ -19,7 +21,6 @@ function checkIsOnline(portal_url) {
             }
         }
         else {
-            console.debug(portal_url, this.readyState, requestPortalSkyHash.status, requestPortalSkyHash.getResponseHeader("skynet-file-metadata"));
             document.getElementById(portal_url + "-online").innerHTML = '&#10060;';
         }
     };
@@ -29,13 +30,19 @@ function checkPortals(portals) {
     document.getElementById('checker').innerHTML = '';
     let html = "";
     for(let i=0; i < portals.length; ++i) {
-        html += "<tr class=\"pure-table-odd\">";						
+        if((i + 1) % 2 === 0) {
+            html += "<tr class=\"pure-table-odd\">";
+        }
+        else {
+            html += "<tr>";
+        }						
         html += "<td>" + portals[i].portal_url + "</td>";
-        html += "<td id=\"" + portals[i].portal_url + "-online\">"+"&#128337;"+"</td>";
-        html += "<td id=\"" + portals[i].portal_url + "-duration\">"+"&#128337;"+"</td>";
+        html += "<td id=\"" + portals[i].portal_url + "-online\">" + "&#128337;" + "</td>";
+        html += "<td id=\"" + portals[i].portal_url + "-duration\">" + "&#128337;" + "</td>";
         html += "<td>" + (portals[i].portal_upload_path ? "&#9989;" : "&#10060") + "</td>";
         html += "<td>" + portals[i].comment + "</td>";
         html += "</tr>";
+
         checkIsOnline(portals[i].portal_url);
     }
     
